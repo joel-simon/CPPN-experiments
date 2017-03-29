ae2b7c3d7f5431e531edd0d1ede6fadf886dd5c6
Joel Simon <joelsimon6@gmail.com>
2017-03-29 09:53:07 -0400

    new congif and 16x16 grid

--- plot.py
+++ plot.py
@@ -3,7 +3,9 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pickle.load(open('results.p', 'rb'))
+names = ['checkerboard', 'R', 'smile', 'random']
+
+data = pickle.load(open('cppn_results.p', 'rb'))
 
 means = data.mean(axis=2).T
 stds = data.std(axis=2).T
@@ -11,29 +13,36 @@ stds = data.std(axis=2).T
 fig, ax = plt.subplots()
 
 index = np.arange(data.shape[0])
-bar_width = 0.25
+bar_width = 0.2
 
 opacity = 0.4
 error_config = {'ecolor': '0.3'}
 
-rects1 = plt.bar(index-bar_width, means[0], bar_width,
+plt.bar(index-2*bar_width, means[0], bar_width,
                  alpha=opacity,
                  color='b',
                  yerr=stds[0],
                  error_kw=error_config,
-                 label='frame')
+                 label='checkerboard')
 
-rects2 = plt.bar(index, means[1], bar_width,
+plt.bar(index-bar_width, means[1], bar_width,
+                 alpha=opacity,
+                 color='g',
+                 yerr=stds[1],
+                 error_kw=error_config,
+                 label='R')
+
+plt.bar(index, means[2], bar_width,
                  alpha=opacity,
                  color='r',
-                 yerr=stds[1],
+                 yerr=stds[2],
                  error_kw=error_config,
                  label='smile')
 
-rects2 = plt.bar(index + bar_width, means[2], bar_width,
+plt.bar(index + bar_width, means[3], bar_width,
                  alpha=opacity,
-                 color='g',
-                 yerr=stds[2],
+                 color='y',
+                 yerr=stds[3],
                  error_kw=error_config,
                  label='random')
 
@@ -41,7 +50,7 @@ plt.xlabel('Num Steps')
 plt.ylabel('Scores')
 plt.title('Scores by num_steps and pattern')
 plt.ylim([0,1.1])
-plt.xticks(index + bar_width / 2, map(str, range(1,6)))
+plt.xticks(index + bar_width / 2, map(str, range(data.shape[0])))
 plt.legend()
 plt.tight_layout()
 plt.show()
